0c97410163fe2dc48ddc617d1b256ce493df65e8
zeeshanhs <z.h.siddiqui20@gmail.com>
2019-03-19 18:57:14 +0500

    Scale all numeric features of data

--- finding_donors.py
+++ finding_donors.py
@@ -20,6 +20,8 @@ from sklearn.svm import SVC
 from sklearn.model_selection import learning_curve
 from sklearn.model_selection import ShuffleSplit
 from IPython import get_ipython
+# Import sklearn.preprocessing.StandardScaler
+from sklearn.preprocessing import MinMaxScaler
 
 from IPython.display import display # Allows the use of display() for DataFrames
 
@@ -69,3 +71,14 @@ features_log_transformed[skewed] = features_raw[skewed].apply(lambda x: np.log(x
 
 # Visualize the new log distributions
 # vs.distribution(features_log_transformed, transformed = True)
+
+# Initialize a scaler, then apply it to the features
+scaler = MinMaxScaler() # default=(0, 1)
+numerical = ['age', 'education-num', 'capital-gain', 'capital-loss', 'hours-per-week']
+
+features_log_minmax_transform = pd.DataFrame(data = features_log_transformed)
+features_log_minmax_transform[numerical] = scaler.fit_transform(features_log_transformed[numerical])
+
+# Show an example of a record with scaling applied
+display(features_log_minmax_transform.head(n = 5))
+
